d5df450d742e00e71dfff02382c09ebf534e307e
Terrarianium <Collinrogers915@gmail.com>
2025-09-03 14:26:52 -0400

    scale and editor class

--- Assets/Object Behavior.cs
+++ Assets/Object Behavior.cs
@@ -1,8 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class ObjectBehavior : MonoBehaviour
 {
     public float size;
+
+    [ExecuteInEditMode]
+    private void Update()
+    {
+        transform.localScale = new Vector3(size, size, size);
+    }
+}
+
+public class ObjectBehaviorEditor : Editor
+{
+
 }
