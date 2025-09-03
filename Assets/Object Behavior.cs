780d5f69e4a9d0248ac22f5ba9db3ef672fb12aa
William Hamaric <wi652643@ucf.edu>
2025-09-03 14:46:02 -0400

    buttons

--- Assets/Object Behavior.cs
+++ Assets/Object Behavior.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEditor.TerrainTools;
 using UnityEngine;
@@ -15,10 +16,32 @@ public class ObjectBehavior : MonoBehaviour
     }
 }
 
+[CustomEditor(typeof(ObjectBehavior)), CanEditMultipleObjects]
 public class ObjectBehaviorEditor : Editor
 {
     public override void OnInspectorGUI()
     {
+        base.OnInspectorGUI();
 
+        if (GUILayout.Button("Select all shapes"))
+        {
+            var allShapes = GameObject.FindObjectsOfType<ObjectBehavior>();
+            var allShapeGameObjects = allShapes.Select(shape  => shape.gameObject).ToArray();
+            Selection.objects = allShapeGameObjects;
+        }
+        EditorGUILayout.BeginHorizontal();
+        if (GUILayout.Button("Select all spheres"))
+        {
+            var allSpheres = GameObject.FindObjectsOfType<SphereCollider>();
+            var allSphereGameObjets = allSpheres.Select(sphere => sphere.gameObject).ToArray();
+            Selection.objects = allSphereGameObjets;
+        }
+        if (GUILayout.Button("Select all cubes"))
+        {
+            var allCubes = GameObject.FindObjectsOfType<BoxCollider>();
+            var allCubeGameObjects = allCubes.Select(cube => cube.gameObject).ToArray();
+            Selection.objects = allCubeGameObjects;
+        }
+        EditorGUILayout.EndHorizontal();
     }
 }
