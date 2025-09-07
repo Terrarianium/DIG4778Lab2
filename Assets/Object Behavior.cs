9863ff580250ad792f1e874b7ec6430d8d8377d1
Hamaric <angryearl@gmail.com>
2025-09-07 15:42:54 -0400

    Update Object Behavior.cs
    Changed how the disable/enable buttons work, added disable/enable for just spheres or cubes, and commented.

--- Assets/Object Behavior.cs
+++ Assets/Object Behavior.cs
@@ -8,6 +8,7 @@ using UnityEditor.TerrainTools;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+// This class handles changing the size of the shapes
 [ExecuteInEditMode]
 public class ObjectBehavior : MonoBehaviour
 {
@@ -32,6 +33,7 @@ public class ObjectBehaviorEditor : Editor
         EnableDisable();
     }
 
+    // This will give a warning in the editor when the size is above 2 and will give an error when the size is below 0
     public void SizeWarnings()
     {
         var size = serializedObject.FindProperty("size");
@@ -45,8 +47,10 @@ public class ObjectBehaviorEditor : Editor
         }
     }
     
+    // This function contains the editor buttons
     public void SelectionButtons()
     {
+        // We included a button to select all shapes
         if (GUILayout.Button("Select all shapes"))
         {
             var allShapes = GameObject.FindObjectsOfType<ObjectBehavior>();
@@ -54,12 +58,14 @@ public class ObjectBehaviorEditor : Editor
             Selection.objects = allShapeGameObjects;
         }
         EditorGUILayout.BeginHorizontal();
+        // All spheres
         if (GUILayout.Button("Select all spheres"))
         {
             var allSpheres = GameObject.FindObjectsOfType<SphereCollider>();
             var allSphereGameObjets = allSpheres.Select(sphere => sphere.gameObject).ToArray();
             Selection.objects = allSphereGameObjets;
         }
+        // All cubes
         if (GUILayout.Button("Select all cubes"))
         {
             var allCubes = GameObject.FindObjectsOfType<BoxCollider>();
@@ -67,33 +73,72 @@ public class ObjectBehaviorEditor : Editor
             Selection.objects = allCubeGameObjects;
         }
         EditorGUILayout.EndHorizontal();
+        // And clears the selection
+        // This does still leave one shape selected after clicking it, though.
         if (GUILayout.Button("Clear selection"))
         {
-            Selection.objects = new Object[] { (target as ObjectBehavior).gameObject };
+            Selection.objects = new UnityEngine.Object[] { (target as ObjectBehavior).gameObject };
         }
     }
 
+    // And this is the function for disabling and enabling all objects, spheres, or cubes with the color reflecting their state
     public void EnableDisable()
     {
-        foreach (GameObject obj in Selection.objects)
-        {
-            if (obj.activeInHierarchy)
-            {
-                GUI.backgroundColor = Color.green;
-                break;
-            } else
-            {
-                GUI.backgroundColor = Color.grey;
-                break;
-            }
-        }
-        
+        // Get all objects
+        var allShapes = GameObject.FindObjectsOfType<ObjectBehavior>(true);
+        var allSpheres = GameObject.FindObjectsOfType<SphereCollider>(true);
+        var allCubes = GameObject.FindObjectsOfType<BoxCollider>(true);
+
+        // Determine if all are enabled/disabled
+        bool allShapesEnabled = allShapes.Length > 0 && allShapes.All(obj => obj.gameObject.activeSelf);
+        bool allSpheresEnabled = allSpheres.Length > 0 && allSpheres.All(obj => obj.gameObject.activeSelf);
+        bool allCubesEnabled = allCubes.Length > 0 && allCubes.All(obj => obj.gameObject.activeSelf);
+
+        // If both spheres and cubes are disabled, allShapes should be considered disabled. This affects the "all shapes" button because it should be greyed out if either spheres and cubes are disabled.
+        bool bothSpheresAndCubesDisabled = !allSpheresEnabled && !allCubesEnabled;
+
+        // All shapes button
+        Color prevColor = GUI.backgroundColor;
+        // This line is a bit complex, but it ensures that the "all shapes" button is greyed out if both spheres and cubes are disabled
+        GUI.backgroundColor = (allShapesEnabled && !bothSpheresAndCubesDisabled) ? Color.green : Color.grey;
         if (GUILayout.Button("Disable/Enable Objects", GUILayout.Height(40)))
         {
-            foreach (var obj in GameObject.FindObjectsOfType<ObjectBehavior>(true))
+            bool newState = !allShapesEnabled;
+            foreach (var obj in allShapes)
             {
-                obj.gameObject.SetActive(!obj.gameObject.activeSelf);
+                obj.gameObject.SetActive(newState);
             }
         }
+        GUI.backgroundColor = prevColor;
+
+        EditorGUILayout.BeginHorizontal();
+
+        // Spheres button
+        // Similar logic as above, but only for spheres, it handles color based on their state
+        GUI.backgroundColor = allSpheresEnabled ? Color.green : Color.grey;
+        if (GUILayout.Button("Disable/Enable Spheres", GUILayout.Height(40)))
+        {
+            bool newState = !allSpheresEnabled;
+            foreach (var obj in allSpheres)
+            {
+                obj.gameObject.SetActive(newState);
+            }
+        }
+        GUI.backgroundColor = prevColor;
+
+        // Cubes button
+        // Similar logic as above, but only for cubes, it handles color based on their state
+        GUI.backgroundColor = allCubesEnabled ? Color.green : Color.grey;
+        if (GUILayout.Button("Disable/Enable Cubes", GUILayout.Height(40)))
+        {
+            bool newState = !allCubesEnabled;
+            foreach (var obj in allCubes)
+            {
+                obj.gameObject.SetActive(newState);
+            }
+        }
+        GUI.backgroundColor = prevColor;
+
+        EditorGUILayout.EndHorizontal();
     }
 }
